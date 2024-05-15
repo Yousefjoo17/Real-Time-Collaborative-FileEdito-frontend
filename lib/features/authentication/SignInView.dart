a3d92f6d6143e39fc02350d6d330b462a6bab7e6
Youssef <yousefyousefyousef335@gmail.com>
2024-05-16 00:55:40 +0300

    complete login and signin

--- lib/features/authentication/SignInView.dart
+++ lib/features/authentication/SignInView.dart
@@ -8,6 +8,7 @@ import 'package:fileeditor/core/widgets/Custom_text_field.dart';
 import 'package:fileeditor/main.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class SignInView extends StatefulWidget {
   const SignInView({super.key});
@@ -18,83 +19,107 @@ class SignInView extends StatefulWidget {
 
 class _SignInViewState extends State<SignInView> {
   UserModel userModel = UserModel();
+  bool isLoading = false;
+  GlobalKey<FormState> formkey = GlobalKey();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 60),
-            Image.asset(AssetsData.logoPath2),
-            const SizedBox(height: 12),
-            SizedBox(
-              width: MediaQuery.of(context).size.width / 2.5,
-              child: Column(
-                children: [
-                  const Text(
-                    "Let's collaborate and share our ideas with others!",
-                    style: TextStyle(
-                      fontFamily: "Pacifico-Regular",
-                      fontSize: 25,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 12),
-                  CustomTextField(
-                    hinttext: "username",
-                    onchanged: (p0) {
-                      userModel.username = p0;
-                    },
-                  ),
-                  const SizedBox(height: 12),
-                  CustomTextField(
-                    hinttext: "password",
-                    obscureText: true,
-                    onchanged: (p0) {
-                      userModel.password = p0;
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  CustomButton(
-                    text: "Sign in",
-                    ontap: () async {
-                      List<UserModel> users = await UserService().getAllUsers();
-                      bool found = UserService().isUserExist(users, userModel);
-                      if (found) {
-                        currUserModel = userModel;
-                        currUserModel.userID =
-                            UserService().getCurrUserID(users, userModel);
-                        showmySnackBar(context, "welcome");
-                        GoRouter.of(context).push(AppRouter.khomeView);
-                      } else {
-                        showmySnackBar(
-                            context, "username or password is incorrect");
-                      }
-                    },
-                    color: Colors.purple,
-                  ),
-                  const SizedBox(height: 12),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        body: Form(
+          key: formkey,
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 60),
+                Image.asset(AssetsData.logoPath2),
+                const SizedBox(height: 12),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 2.5,
+                  child: Column(
                     children: [
-                      const Text("Don't have an account? "),
-                      GestureDetector(
-                        child: const Text(
-                          "Sign Up",
-                          style: TextStyle(color: Colors.purple),
-                          textAlign: TextAlign.center,
+                      const Text(
+                        "Let's collaborate and share our ideas with others!",
+                        style: TextStyle(
+                          fontFamily: "Pacifico-Regular",
+                          fontSize: 25,
                         ),
-                        onTap: () {
-                          GoRouter.of(context).push(AppRouter.ksignupView);
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 12),
+                      CustomTextField(
+                        hinttext: "username",
+                        onchanged: (p0) {
+                          userModel.username = p0;
                         },
+                      ),
+                      const SizedBox(height: 12),
+                      CustomTextField(
+                        hinttext: "password",
+                        obscureText: true,
+                        onchanged: (p0) {
+                          userModel.password = p0;
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      CustomButton(
+                        text: "Sign in",
+                        ontap: () async {
+                          if (formkey.currentState!.validate()) {
+                            setState(() {
+                              isLoading = true;
+                            });
+                            List<UserModel> users =
+                                await UserService().getAllUsers();
+                            bool found =
+                                UserService().isUserExist(users, userModel);
+                            if (found) {
+                              currUserModel = userModel;
+                              currUserModel.userID =
+                                  UserService().getCurrUserID(users, userModel);
+                              showmySnackBar(context, "welcome");
+                              GoRouter.of(context).push(AppRouter.khomeView);
+                              setState(() {
+                                isLoading = false;
+                              });
+                            } else {
+                              showmySnackBar(
+                                  context, "username or password is incorrect");
+                              setState(() {
+                                isLoading = false;
+                              });
+                            }
+                          }
+                          setState(() {
+                            isLoading = false;
+                          });
+                        },
+                        color: Colors.purple,
+                      ),
+                      const SizedBox(height: 12),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text("Don't have an account? "),
+                          GestureDetector(
+                            child: const Text(
+                              "Sign Up",
+                              style: TextStyle(color: Colors.purple),
+                              textAlign: TextAlign.center,
+                            ),
+                            onTap: () {
+                              GoRouter.of(context).push(AppRouter.ksignupView);
+                            },
+                          )
+                        ],
                       )
                     ],
-                  )
-                ],
-              ),
-            )
-          ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
--- lib/features/authentication/SignUpView.dart
+++ lib/features/authentication/SignUpView.dart
@@ -8,6 +8,7 @@ import 'package:fileeditor/core/widgets/Custom_text_field.dart';
 import 'package:fileeditor/main.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class SignUpView extends StatefulWidget {
   const SignUpView({super.key});
@@ -18,84 +19,107 @@ class SignUpView extends StatefulWidget {
 
 class _SignUpViewState extends State<SignUpView> {
   UserModel userModel = UserModel();
+  bool isLoading = false;
+  GlobalKey<FormState> formkey = GlobalKey();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 60),
-            Image.asset(AssetsData.logoPath1),
-            const SizedBox(height: 12),
-            SizedBox(
-              width: MediaQuery.of(context).size.width / 2.5,
-              child: Column(
-                children: [
-                  const Text(
-                    "Let's collaborate and share our ideas with others!",
-                    style: TextStyle(
-                      fontFamily: "Pacifico-Regular",
-                      fontSize: 25,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  CustomTextField(
-                    hinttext: "username",
-                    onchanged: (p0) {
-                      userModel.username = p0;
-                    },
-                  ),
-                  const SizedBox(height: 12),
-                  CustomTextField(
-                    hinttext: "password",
-                    obscureText: true,
-                    onchanged: (p0) {
-                      userModel.password = p0;
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  CustomButton(
-                    text: "Sign Up",
-                    ontap: () async {
-                      List<UserModel> users = await UserService().getAllUsers();
-                      bool found =
-                          UserService().isUsernameExists(users, userModel);
-                      if (found) {
-                        showmySnackBar(context, "user is already found");
-                      } else {
-                        await UserService().createUser(userModel);
-
-                        currUserModel = userModel;
-                        currUserModel.userID = users.length + 1;
-                        showmySnackBar(context,
-                            "regsiterd successdully ${userModel.username}");
-                        GoRouter.of(context).push(AppRouter.khomeView);
-                      }
-                    },
-                    color: Colors.purple,
-                  ),
-                  const SizedBox(height: 12),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        body: Form(
+          key: formkey,
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 60),
+                Image.asset(AssetsData.logoPath1),
+                const SizedBox(height: 12),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 2.5,
+                  child: Column(
                     children: [
-                      const Text("Already have an account? "),
-                      GestureDetector(
-                        child: const Text(
-                          "Sign in",
-                          style: TextStyle(color: Colors.purple),
-                          textAlign: TextAlign.center,
+                      const Text(
+                        "Let's collaborate and share our ideas with others!",
+                        style: TextStyle(
+                          fontFamily: "Pacifico-Regular",
+                          fontSize: 25,
                         ),
-                        onTap: () {
-                          GoRouter.of(context).push(AppRouter.ksigninView);
+                        textAlign: TextAlign.center,
+                      ),
+                      CustomTextField(
+                        hinttext: "username",
+                        onchanged: (p0) {
+                          userModel.username = p0;
                         },
+                      ),
+                      const SizedBox(height: 12),
+                      CustomTextField(
+                        hinttext: "password",
+                        obscureText: true,
+                        onchanged: (p0) {
+                          userModel.password = p0;
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      CustomButton(
+                        text: "Sign Up",
+                        ontap: () async {
+                          if (formkey.currentState!.validate()) {
+                            setState(() {
+                              isLoading = true;
+                            });
+                            List<UserModel> users =
+                                await UserService().getAllUsers();
+                            bool found = UserService()
+                                .isUsernameExists(users, userModel);
+                            if (found) {
+                              showmySnackBar(context, "user is already found");
+                              setState(() {
+                                isLoading = false;
+                              });
+                            } else {
+                              await UserService().createUser(userModel);
+
+                              currUserModel = userModel;
+                              currUserModel.userID = users.length + 1;
+                              showmySnackBar(context,
+                                  "regsiterd successdully ${userModel.username}");
+                              GoRouter.of(context).push(AppRouter.khomeView);
+                              setState(() {
+                                isLoading = false;
+                              });
+                            }
+                            setState(() {
+                              isLoading = false;
+                            });
+                          }
+                        },
+                        color: Colors.purple,
+                      ),
+                      const SizedBox(height: 12),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text("Already have an account? "),
+                          GestureDetector(
+                            child: const Text(
+                              "Sign in",
+                              style: TextStyle(color: Colors.purple),
+                              textAlign: TextAlign.center,
+                            ),
+                            onTap: () {
+                              GoRouter.of(context).push(AppRouter.ksigninView);
+                            },
+                          )
+                        ],
                       )
                     ],
-                  )
-                ],
-              ),
-            )
-          ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
