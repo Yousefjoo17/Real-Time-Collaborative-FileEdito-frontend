617b5ffd068a37899e97e1785d4d57c0342b8173
Youssef <yousefyousefyousef335@gmail.com>
2024-05-16 04:39:12 +0300

    permisions for more features

--- lib/features/FileManagment/widgets/OtherOptionsbuttom.dart
+++ lib/features/FileManagment/widgets/OtherOptionsbuttom.dart
@@ -1,7 +1,10 @@
+import 'package:fileeditor/core/helpers/CustomShowSnackBar.dart';
 import 'package:fileeditor/core/models/FileModel.dart';
+import 'package:fileeditor/core/services/UserFilePermissionService.dart';
 import 'package:fileeditor/core/services/fileService.dart';
 import 'package:fileeditor/core/utils/AppRouter.dart';
 import 'package:fileeditor/features/FileManagment/widgets/RenameButton.dart';
+import 'package:fileeditor/main.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -14,39 +17,44 @@ class OtherOptionsButton extends StatelessWidget {
     return IconButton(
       icon: const Icon(Icons.more_vert),
       onPressed: () {
-        final RenderBox renderBox = context.findRenderObject() as RenderBox;
-        final Offset position = renderBox.localToGlobal(Offset.zero);
-        final Size size = renderBox.size;
-        showMenu(
-          context: context,
-          position: RelativeRect.fromLTRB(
-            position.dx,
-            position.dy + size.height, // Place menu below the button
-            position.dx + size.width,
-            position.dy + size.height + 100, // Adjust as needed
-          ),
-          items: [
-            PopupMenuItem(
-              child: RenameButton(fileModel: fileModel),
+        if (UserFilePermissionService()
+            .isOwner(fileModel.fileID!, currUserModel.userID!)) {
+          final RenderBox renderBox = context.findRenderObject() as RenderBox;
+          final Offset position = renderBox.localToGlobal(Offset.zero);
+          final Size size = renderBox.size;
+          showMenu(
+            context: context,
+            position: RelativeRect.fromLTRB(
+              position.dx,
+              position.dy + size.height, // Place menu below the button
+              position.dx + size.width,
+              position.dy + size.height + 100, // Adjust as needed
             ),
-            PopupMenuItem(
-              child: ElevatedButton(
-                onPressed: () async {
-                  await FileService().deleteFile(fileModel.fileID!);
-                  GoRouter.of(context).pop();
-                  GoRouter.of(context).push(AppRouter.khomeView);
-                },
-                child: const Row(
-                  children: [
-                    Icon(Icons.remove_done_outlined),
-                    Spacer(),
-                    Text("Remove"),
-                  ],
+            items: [
+              PopupMenuItem(
+                child: RenameButton(fileModel: fileModel),
+              ),
+              PopupMenuItem(
+                child: ElevatedButton(
+                  onPressed: () async {
+                    await FileService().deleteFile(fileModel.fileID!);
+                    GoRouter.of(context).pop();
+                    GoRouter.of(context).push(AppRouter.khomeView);
+                  },
+                  child: const Row(
+                    children: [
+                      Icon(Icons.remove_done_outlined),
+                      Spacer(),
+                      Text("Remove"),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
-        );
+            ],
+          );
+        } else {
+          showmySnackBar(context, "You are not an owner");
+        }
       },
     );
   }
--- lib/features/FileManagment/widgets/ShareButton.dart
+++ lib/features/FileManagment/widgets/ShareButton.dart
@@ -4,6 +4,7 @@ import 'package:fileeditor/core/models/UserFilePermission.dart';
 import 'package:fileeditor/core/models/UserModel.dart';
 import 'package:fileeditor/core/services/UserFilePermissionService.dart';
 import 'package:fileeditor/core/services/userService.dart';
+import 'package:fileeditor/main.dart';
 import 'package:flutter/material.dart';
 
 class ShareButton extends StatefulWidget {
@@ -24,96 +25,102 @@ class _ShareButtonState extends State<ShareButton> {
   Widget build(BuildContext context) {
     return IconButton(
       onPressed: () {
-        final RenderBox renderBox = context.findRenderObject() as RenderBox;
-        final Offset position = renderBox.localToGlobal(Offset.zero);
-        final Size size = renderBox.size;
-        showMenu(
-          context: context,
-          position: RelativeRect.fromLTRB(
-            position.dx,
-            position.dy + size.height, // Place menu below the button
-            position.dx + size.width,
-            position.dy + size.height + 100, // Adjust as needed
-          ),
-          items: [
-            PopupMenuItem(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SizedBox(
-                    width: 90,
-                    height: 30,
-                    child: TextField(
-                      onChanged: (value) {
-                        userName = value;
-                      },
-                      decoration: const InputDecoration(
-                        hintText: "name",
+        if (UserFilePermissionService()
+            .isOwner(widget.fileModel.fileID!, currUserModel.userID!)) {
+          final RenderBox renderBox = context.findRenderObject() as RenderBox;
+          final Offset position = renderBox.localToGlobal(Offset.zero);
+          final Size size = renderBox.size;
+          showMenu(
+            context: context,
+            position: RelativeRect.fromLTRB(
+              position.dx,
+              position.dy + size.height, // Place menu below the button
+              position.dx + size.width,
+              position.dy + size.height + 100, // Adjust as needed
+            ),
+            items: [
+              PopupMenuItem(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      width: 90,
+                      height: 30,
+                      child: TextField(
+                        onChanged: (value) {
+                          userName = value;
+                        },
+                        decoration: const InputDecoration(
+                          hintText: "name",
+                        ),
                       ),
                     ),
-                  ),
-                  Column(
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          permission = 0;
-                        },
-                        child: const Text("Owner"),
-                      ),
-                      const SizedBox(height: 12),
-                      ElevatedButton(
-                        onPressed: () {
-                          permission = 1;
-                        },
-                        child: const Text("Editor"),
-                      ),
-                      const SizedBox(height: 12),
-                      ElevatedButton(
-                        onPressed: () {
-                          permission = 2;
-                        },
-                        child: const Text("Viewer"),
-                      ),
-                    ],
-                  ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      UserModel userModel = UserModel(username: userName);
+                    Column(
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            permission = 0;
+                          },
+                          child: const Text("Owner"),
+                        ),
+                        const SizedBox(height: 12),
+                        ElevatedButton(
+                          onPressed: () {
+                            permission = 1;
+                          },
+                          child: const Text("Editor"),
+                        ),
+                        const SizedBox(height: 12),
+                        ElevatedButton(
+                          onPressed: () {
+                            permission = 2;
+                          },
+                          child: const Text("Viewer"),
+                        ),
+                      ],
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        UserModel userModel = UserModel(username: userName);
 
-                      List<UserModel> users = await UserService().getAllUsers();
+                        List<UserModel> users =
+                            await UserService().getAllUsers();
 
-                      bool NameISfound =
-                          UserService().isUsernameExists(users, userModel);
+                        bool nameISfound =
+                            UserService().isUsernameExists(users, userModel);
 
-                      int? userid =
-                          UserService().getUserIDbyName(users, userName);
+                        int? userid =
+                            UserService().getUserIDbyName(users, userName);
 
-                      UserFilePermission userFilePermission =
-                          UserFilePermission(
-                        fileId: widget.fileModel.fileID,
-                        userId: userid,
-                        permisson: permission,
-                      );
+                        UserFilePermission userFilePermission =
+                            UserFilePermission(
+                          fileId: widget.fileModel.fileID,
+                          userId: userid,
+                          permisson: permission,
+                        );
 
-                      if (NameISfound) {
-                        await UserFilePermissionService()
-                            .createUserFilePermission(userFilePermission);
-                        showmySnackBar(
-                            context, "permission was given successfully");
-                      } else {
-                        showmySnackBar(context, "User is not found");
-                      }
-                    },
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.purple)),
-                    child: const Text("Save "),
-                  )
-                ],
+                        if (nameISfound) {
+                          await UserFilePermissionService()
+                              .createUserFilePermission(userFilePermission);
+                          showmySnackBar(
+                              context, "permission was given successfully");
+                        } else {
+                          showmySnackBar(context, "User is not found");
+                        }
+                      },
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.purple)),
+                      child: const Text("Save "),
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
-        );
+            ],
+          );
+        } else {
+          showmySnackBar(context, "You are not an owner");
+        }
       },
       icon: const Icon(
         Icons.mobile_screen_share_sharp,
